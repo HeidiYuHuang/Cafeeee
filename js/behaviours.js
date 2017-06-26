550d4d9ccf3dfdd64ccc8ca90bfa0c9b8f0dd32f
Unknown <yha113@sfu.ca>
2017-06-25 23:13:18 -0700

    adjust menu dropdown

--- js/behaviours.js
+++ js/behaviours.js
@@ -1,3 +1,21 @@
-function dropdownSubnav() {
-	document.getElementById("header-subnav-dropdown").classList.toggle("header-nav-sub");
+function widthChange() {
+	if (window.innerWidth <= 656 && window.innerWidth > 416) {
+		document.getElementById("header-subnav-dropdown").style.display = 'block';
+	} else {
+		document.getElementById("header-subnav-dropdown").style.display = 'none';
+	}
+
+	if (window.innerWidth <= 416) {
+		document.getElementById("header-nav-icon").style.display = 'flex';
+	} else {
+		document.getElementById("header-nav-icon").style.display = 'none';
+	}
+}
+
+function dropdownSubnav () {
+	if (document.getElementById("header-subnav-dropdown").style.display == 'block') {
+		document.getElementById("header-subnav-dropdown").style.display = 'none';
+	} else {
+		document.getElementById("header-subnav-dropdown").style.display = 'block';
+	}
 }
